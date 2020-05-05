55a45c7feb9dc68b3e7db64f51c8f9d0ec7cbd92
farizalbab <farizalbab@gmail.com>
2020-05-05 14:57:43 +0700

    Updates

---
+++ precache-manifest.48e82864df7b34c60066a679f97a1474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa76ca8b4609f81c36fa4e6ebe72aed",
+    "url": "/seger-waras/index.html"
+  },
+  {
+    "revision": "60715361a416bb06da26",
+    "url": "/seger-waras/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "97722a0b125f736cbd57",
+    "url": "/seger-waras/static/css/main.893cf1a5.chunk.css"
+  },
+  {
+    "revision": "60715361a416bb06da26",
+    "url": "/seger-waras/static/js/2.899a118c.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/seger-waras/static/js/2.899a118c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97722a0b125f736cbd57",
+    "url": "/seger-waras/static/js/main.4354b8a7.chunk.js"
+  },
+  {
+    "revision": "2d35692253ff9f0824cd",
+    "url": "/seger-waras/static/js/runtime-main.a9a6222c.js"
+  }
+]);
