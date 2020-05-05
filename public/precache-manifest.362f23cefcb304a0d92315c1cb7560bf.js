97f97ffd02e7f224b5dc286484f2bfb08004ed93
alfarelzaki <alfarell.zaki@gmail.com>
2020-05-05 17:09:46 +0700

    final

---
+++ public/precache-manifest.362f23cefcb304a0d92315c1cb7560bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8b07fe5d7531fc9be3eeae8ca2c257",
+    "url": "/seger-waras/index.html"
+  },
+  {
+    "revision": "60715361a416bb06da26",
+    "url": "/seger-waras/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "cc5bad6e125599917d4e",
+    "url": "/seger-waras/static/css/main.893cf1a5.chunk.css"
+  },
+  {
+    "revision": "60715361a416bb06da26",
+    "url": "/seger-waras/static/js/2.899a118c.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/seger-waras/static/js/2.899a118c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5bad6e125599917d4e",
+    "url": "/seger-waras/static/js/main.4c1129af.chunk.js"
+  },
+  {
+    "revision": "2d35692253ff9f0824cd",
+    "url": "/seger-waras/static/js/runtime-main.a9a6222c.js"
+  }
+]);
---
+++ public/precache-manifest.41aed6e09ea6864bb9917af360044ff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6afeb448079fd4bed1b69aa2c423e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "042a43297634d25e17ec",
+    "url": "/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "d41fc9dcd3731c79c110",
+    "url": "/static/css/main.893cf1a5.chunk.css"
+  },
+  {
+    "revision": "042a43297634d25e17ec",
+    "url": "/static/js/2.bd0e51c8.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/static/js/2.bd0e51c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41fc9dcd3731c79c110",
+    "url": "/static/js/main.f88769c3.chunk.js"
+  },
+  {
+    "revision": "979cee976c128d1131b7",
+    "url": "/static/js/runtime-main.56a7690f.js"
+  }
+]);
